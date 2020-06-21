f663ca55f448a37e61a771307b0b563881336392
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-06-21 17:25:45 +0530

    Added Non Scrolling Version

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	ScrollView,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+	};
+
+	return (
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title="ADD" onPress={addGoalHandler} />
+			</View>
+			<View>
+				{courseGoals.map((goal) => (
+					<View key={goal} style={styles.listItem}>
+						<Text>{goal}</Text>
+					</View>
+				))}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		padding: 50,
+	},
+
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+
+	input: {
+		borderColor: "#000",
+		borderWidth: 1,
+		padding: 10,
+		width: "80%",
+	},
+
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		borderWidth: 1,
+		borderColor: "#000",
+		backgroundColor: "#ccc",
+	},
 });
